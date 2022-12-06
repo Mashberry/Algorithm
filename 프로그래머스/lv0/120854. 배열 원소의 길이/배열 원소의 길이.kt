ca131a50589466061eec19beae1a42df19e2bd36
Mashberry <ksjhs85@gmail.com>
2022-12-06 18:17:00 +0900

    [level 0] Title: 배열 원소의 길이, Time: 5.55 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(strlist: Array<String>): IntArray {
+        var answer: IntArray = strlist.map { it.length }.toIntArray()
+        
+        return answer
+    }
+}
