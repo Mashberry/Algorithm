6a7d55f5cb94f3ff0d2f9859e498ce01bcd13f78
Mashberry <ksjhs85@gmail.com>
2022-12-06 18:09:41 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var result = 0
+        
+        for(i in 1..n) {
+            if(n % i == 0) result += i
+        }
+        
+        return result
+    }
+}
