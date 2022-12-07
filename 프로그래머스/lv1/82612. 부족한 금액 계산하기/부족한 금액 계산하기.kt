2b9a23fb010db10964b6e4c2a15480711a69c835
Mashberry <ksjhs85@gmail.com>
2022-12-08 02:41:57 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 1.91 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun solution(price: Int, money: Int, count: Int): Long {
+        return (1..count).sumOf { price.toLong() * it }.let { if(money.toLong() >= it) 0 else it - money.toLong() }
+    }
+}
