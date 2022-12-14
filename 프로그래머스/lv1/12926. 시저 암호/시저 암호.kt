1c9d0546a628a64c9b6a1a637a60b7d2ff783330
Mashberry <ksjhs85@gmail.com>
2022-12-14 22:30:55 +0900

    [level 1] Title: 시저 암호, Time: 17.38 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.kt
@@ -1,26 +1,11 @@
 class Solution {
     fun solution(s: String, n: Int): String {
-        var result = ""
-        val lowerMin = 'a'.code
-        val lowerMax = 'z'.code
-        val lowerRange = lowerMax - lowerMin + 1
-        val lowerRegex = "^[a-z]+$".toRegex()
-        val upperMin = 'A'.code
-        val upperMax = 'Z'.code
-        val upperRange = upperMax - upperMin + 1
-        val upperRegex = "^[A-Z]+$".toRegex()
-
-        for (c in s) {
-            val isUpper = c.toString().matches(upperRegex)
-            val plusStr = (c.code + n).toChar().toString()
-            result +=
-                if (c == ' ') c
-                else if (isUpper) {
-                    if (plusStr.matches(upperRegex)) c + n
-                    else ((c.code + n - upperMax) % upperRange + upperMin - 1).toChar()
-                } else if (plusStr.matches(lowerRegex)) c + n
-                else ((c.code + n - lowerMax) % lowerRange + lowerMin - 1).toChar()
-        }
-        return result
+        return s.map {
+            when {
+                it.isLowerCase() -> 'a' + (it + n - 'a') % 26
+                it.isUpperCase() -> 'A' + (it + n - 'A') % 26
+                else -> ' '
+            }
+        }.joinToString("")
     }
 }
