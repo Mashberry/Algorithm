b1fdc309b330a4a7bd99420b04c0f6eba3e26d99
Mashberry <ksjhs85@gmail.com>
2022-12-14 22:45:44 +0900

    [level 1] Title: 시저 암호, Time: 32.80 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.kt
@@ -1,14 +1,10 @@
 class Solution {
     fun solution(s: String, n: Int): String {
-        var result = ""
         val range = 'z' - 'a' + 1
-
-        for (c in s) {
-            result +=
-                if (c == ' ') c
-                else if (c.isUpperCase()) 'A' + (c + n - 'A') % range
-                else 'a' + (c + n - 'a') % range
+        return s.fold("") { acc, c ->
+            acc + if (c == ' ') c
+            else if (c.isUpperCase()) 'A' + (c + n - 'A') % range
+            else 'a' + (c + n - 'a') % range
         }
-        return result
     }
 }
