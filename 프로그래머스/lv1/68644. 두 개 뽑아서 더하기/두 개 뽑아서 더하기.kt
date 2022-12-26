c4b6a8969e4d62a5f8ee4938a59338ba37e12bb7
Mashberry <ksjhs85@gmail.com>
2022-12-26 18:38:13 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 11.64 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun solution(numbers: IntArray): IntArray {
         val size = numbers.size
-        var list = mutableListOf<Int>()
+        var intSet = mutableSetOf<Int>()
         
         for(i in 0 .. size - 2) {
             for(j in i + 1 .. size - 1) {
-                list.add(numbers[i] + numbers[j])
+                intSet.add(numbers[i] + numbers[j])
             }
         }
         
-        return list.distinct().sorted().toIntArray()
+        return intSet.sorted().toIntArray()
     }
 }
