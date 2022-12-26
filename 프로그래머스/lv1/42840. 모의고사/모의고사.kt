6512f1dff4b5bbe3e54e0b5ea54e046862e1ad87
Mashberry <ksjhs85@gmail.com>
2022-12-26 21:29:33 +0900

    [level 1] Title: 모의고사, Time: 42.63 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -1,15 +1,18 @@
 class Solution {
     fun solution(answers: IntArray): IntArray {
-        val aList = listOf(1, 2, 3, 4, 5)
-        val bList = listOf(2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5)
-        val cList = listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        val supojas = listOf(
+            listOf(1, 2, 3, 4, 5),
+            listOf(2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5),
+            listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        )
 
-        val array = Array(3) { Pair(0, 0) }
+        val array = Array(supojas.size) { Pair(0, 0) }
 
         for (i in answers.indices) {
-            if (answers[i] == aList[i % aList.size]) array[0] = Pair(1, array[0].second + 1)
-            if (answers[i] == bList[i % bList.size]) array[1] = Pair(2, array[1].second + 1)
-            if (answers[i] == cList[i % cList.size]) array[2] = Pair(3, array[2].second + 1)
+            for (j in supojas.indices) {
+                if (answers[i] == supojas[j][i % supojas[j].size])
+                    array[j] = Pair(j + 1, array[j].second + 1)
+            }
         }
 
         val list = array.sortedByDescending { it.second }.groupBy { it.second }
