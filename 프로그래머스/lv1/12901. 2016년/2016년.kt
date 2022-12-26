121735ed033f2d731d5ab25eaf4130cc9bc3e7b4
Mashberry <ksjhs85@gmail.com>
2022-12-26 20:13:34 +0900

    [level 1] Title: 2016년, Time: 16.62 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.kt
+++ 프로그래머스/lv1/12901. 2016년/2016년.kt
@@ -2,7 +2,7 @@ class Solution {
     fun solution(a: Int, b: Int): String {
         val month = listOf(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
         val day = listOf("THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED")
-        val count = if (a > 1) (0 until a - 1).foldIndexed(b) { index, acc, i -> acc + month[index] } else b
+        val count = (0 until a - 1).foldIndexed(b) { index, acc, i -> acc + month[index] }
         return day[count % day.size]
     }
 }
